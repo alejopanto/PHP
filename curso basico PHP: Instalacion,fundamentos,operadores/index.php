c800d64b8ae1a137c313f9836bffc3fa0108fa86
alejopanto <jose.pantoja@testu.co>
2023-03-14 12:55:17 -0500

    debug

--- curso basico PHP: Instalacion,fundamentos,operadores/index.php
+++ curso basico PHP: Instalacion,fundamentos,operadores/index.php
@@ -8,4 +8,15 @@ $apellido = "Santana";
 //echo $nombre .' '. $apellido . "\n";
 // echo "$nombre $apellido \n";
 
-echo "El resultado de 4 x 5 es: " . (4 * 5) . "\n";
+// echo "El resultado de 4 x 5 es: " . (4 * 5) . "\n";
+
+$personas = [
+    "Juan" => 22,
+    "pancho" => 15,
+    "pedro" => 23
+];
+
+var_dump( $personas );
+print_r( $personas );
+
+echo "\n";
