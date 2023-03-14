fed544b237bafd5e083e117ea8cc6b781c781300
alejopanto <jose.pantoja@testu.co>
2023-03-13 22:17:14 -0500

    sintaxis

--- curso basico PHP: Instalacion,fundamentos,operadores/index.php
+++ curso basico PHP: Instalacion,fundamentos,operadores/index.php
@@ -1,2 +1,11 @@
 <?php   
-echo "Hola, mundo";
+
+//echo "Hola, mundo \n";
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+//echo $nombre .' '. $apellido . "\n";
+// echo "$nombre $apellido \n";
+
+echo "El resultado de 4 x 5 es: " . (4 * 5) . "\n";
