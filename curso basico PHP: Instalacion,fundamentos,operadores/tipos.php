1721f30461dc67a6742c6baedfe3c32639af1dc9
alejopanto <jose.pantoja@testu.co>
2023-03-23 15:10:19 -0500

    type of data

---
+++ curso basico PHP: Instalacion,fundamentos,operadores/tipos.php
@@ -0,0 +1,17 @@
+<?php
+
+$numerito = 10;
+
+$nuevo_numerito = $numerito + 5;
+$nuevo_float = $numerito + 0.5;
+
+var_dump($numerito);
+var_dump($nuevo_numerito);
+var_dump($nuevo_float);
+
+$texto = "10 papas vendidas de 20";
+$papas = $texto + 5;
+
+echo $papas; #Sumara el primer valor si es numerico (15 + warning)
+
+echo "\n";
