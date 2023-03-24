4ca76e7ff2c11ea29a5edb160fbf9cece67b4f75
alejopanto <jose.pantoja@testu.co>
2023-03-23 22:14:26 -0500

    otrosm operadores

---
+++ curso basico PHP: Instalacion,fundamentos,operadores/otros_operadores.php
@@ -0,0 +1,10 @@
+<?php
+
+# =	Asignación
+# +=	Incremento sobre le mismo valor
+# ++	Incremento +1
+# -=	Decremento sobre el mismo valor
+# --	Decremento -1
+# *=	Multiplicación sobre el mismo valor
+# /=	División sobre el mismo valor
+# .=	Concatenación sobre el mismo valor
