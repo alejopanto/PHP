42826795c96969fbb678dd2ec0d9d9b447a72462
alejopanto <jose.pantoja@testu.co>
2023-03-23 22:03:05 -0500

    operadores relacionales

---
+++ curso basico PHP: Instalacion,fundamentos,operadores/operadores_relacionales.php
@@ -0,0 +1,12 @@
+<?php
+
+// == Igual
+// === Identico
+// != Diferente
+// !== Diferente no identico
+// < Menor que
+// <= Menor o igual
+// > Mayor que
+// >= Mayor o igual
+// <=> Nave espacial
+// ?? Fusion de null
