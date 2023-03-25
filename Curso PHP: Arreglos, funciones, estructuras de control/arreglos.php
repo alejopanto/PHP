c04600387cd57e7a2ebb2651eac0d77890adff09
alejopanto <jose.pantoja@testu.co>
2023-03-25 18:20:53 -0500

    nuevo curso, Arreglos

---
+++ Curso PHP: Arreglos, funciones, estructuras de control/arreglos.php
@@ -0,0 +1,13 @@
+<?php
+
+//$edades = [20, 28, 30];
+$edades = array(20, 18, 30);
+
+echo("Subindice 0: $edades[0]");
+echo("\n");
+echo("Subindice 1: $edades[1]");
+echo("\n");
+echo("Subindice 2: $edades[2]");
+echo("\n");
+
+echo("\n");
