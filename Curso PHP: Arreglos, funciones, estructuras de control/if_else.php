fab440c4de7f4773867272d0007a139ea7890724
alejopanto <jose.pantoja@testu.co>
2023-04-07 10:28:49 -0500

    if_else

---
+++ Curso PHP: Arreglos, funciones, estructuras de control/if_else.php
@@ -0,0 +1,13 @@
+<?php
+
+$asientos = 4;
+
+if ($asientos == 1) {
+    echo('Correle manito');
+} else if($asientos >= 2) {
+    echo('Calmao que si hay!');
+}else {
+    echo('Pues no mi siela');
+}
+
+echo("\n");
