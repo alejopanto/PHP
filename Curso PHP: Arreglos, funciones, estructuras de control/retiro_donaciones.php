e2416096f233de4c882ffa3bfc53307668ea9b73
alejopanto <jose.pantoja@testu.co>
2023-04-08 15:24:37 -0500

    reto 2

---
+++ Curso PHP: Arreglos, funciones, estructuras de control/retiro_donaciones.php
@@ -0,0 +1,11 @@
+<?php
+
+$streamer = readline("Cuantos mangos poseemos? ");
+
+if ($streamer < 100) {
+    echo("No puedes retirar, primero necesitas tener 100 mangos");
+} else {
+    echo("Claro que si mi rey, aca estan las lukas");
+}
+
+echo("\n");
