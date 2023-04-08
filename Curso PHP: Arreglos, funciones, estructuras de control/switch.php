852c54aad214076201004a49ff7cd2644b434c55
alejopanto <jose.pantoja@testu.co>
2023-04-08 15:16:28 -0500

    switch

---
+++ Curso PHP: Arreglos, funciones, estructuras de control/switch.php
@@ -0,0 +1,26 @@
+<?php
+
+$michi = 3;
+
+switch ($michi) {
+    case 1:
+        echo("Su numero favorito es el 9");
+        break;
+    case 2:
+        echo("Su numero favorito es el 12");
+        break;
+    case 3:
+        echo("Su numero favorito es el 2");
+        break;
+    case 4:
+        echo("Su numero favorito es el 8");
+        break;
+    case 5:
+        echo("Su numero favorito es el 16");
+        break;
+    default:
+        echo("Ese michi no ta");
+        break;
+}
+
+echo("\n");
