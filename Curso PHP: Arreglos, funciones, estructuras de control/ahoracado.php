ce83ee9065e4bbc4a5a5c47f3a08371ae1b4a4b2
alejopanto <jose.pantoja@testu.co>
2023-04-15 18:50:23 -0500

    ahoracado v2

--- Curso PHP: Arreglos, funciones, estructuras de control/ahoracado.php
+++ Curso PHP: Arreglos, funciones, estructuras de control/ahoracado.php
@@ -1,5 +1,25 @@
 <?php
 
+function clear()
+{
+    if (PHP_OS == "WINNT") {
+        system("cls");
+    }else {
+        system("clear");
+    }
+}
+
+function str_contains($haystack, $letter)
+{
+    $chars = str_split($haystack);
+    foreach($chars as $char){
+        if($char == $letter){
+            return true;
+        }
+    }
+    return false;
+}
+
 $possible_words = ["Platzi", "Tormenta", "Huracan", "Citas", "Contexto", "Videos", "Perro", "Gato"];
 
 define("MAX_ATTENMPS", 6);
@@ -12,31 +32,39 @@ $word_length = strlen($chosse_word);
 $discovered_letters = str_pad("", $word_length, "_");
 $attemps = 0;
 
-echo "Palabra de $word_length letras \n\n";
-echo $discovered_letters . "\n\n";
+do {
+    echo "Palabra de $word_length letras \n\n";
+    echo $discovered_letters . "\n\n";
 
-$player_letter = strtolower( readline("Escribe una letra: ") );
+    $player_letter = strtolower( readline("Escribe una letra: ") );
 
-function str_contains($haystack, $letter)
-{
-   $chars = str_split($haystack);
-   foreach($chars as $char){
-    if($char == $letter){
-        return true;
+    if ( str_contains($chosse_word, $player_letter) ) {
+        $offset = 0;
+        while( 
+            ($letter_position = strpos($chosse_word, $player_letter, $offset)) !== false    
+        ) {
+            $discovered_letters[$letter_position] = $player_letter;
+            $offset = $letter_position + 1;
+        }
+    } else {
+        clear();
+        $attemps++;
+        echo "Letra incorrecta !, te quedan " . (MAX_ATTENMPS - $attemps) . " intentos.";
+        sleep(2);
     }
-   }
-   return false;
-}
 
-if ( str_contains($chosse_word, $player_letter) ) {
-    $offset = 0;
-    while( $letter_position = strpos($chosse_word, $player_letter, $offset) !== false ) {
-        $discovered_letters[$letter_position] = $player_letter;
-        $offset = $letter_position + 1;
-    }
+    clear();
+} while ($attemps < MAX_ATTENMPS && $discovered_letters != $chosse_word);
+
+clear();
+
+if ($attemps < MAX_ATTENMPS) {
+    echo "Felicidades adivinaste \n\n";
 } else {
-    $attemps++;
-    echo "Letra incorrecta !, te quedan " . (MAX_ATTENMPS - $attemps) . " intentos.";
+    echo "Suerte para la proxima \n\n";
 }
+
+echo "La parabra es: $chosse_word \n\n";
+echo "Tu pusiste: $discovered_letters \n\n";
 
 echo("\n");
